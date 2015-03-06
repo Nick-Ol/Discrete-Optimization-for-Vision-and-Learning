d586f12e6ebdfced5ace72471dd2249fdd1d4bca
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-03-06 17:41:13 +0100

    Beginning
    Get an error on subscript indices

--- Assignment 1/assignment.m
+++ Assignment 1/assignment.m
@@ -22,11 +22,41 @@ figure(2); clf(2); imagesc(disparity); colormap(gray); title('Disparity Image - 
 img_left  = convertToGray(img_left);
 img_right = convertToGray(img_right);
 
-
+%%
 % TODO : implement the alpha-beta swap algorithm here. 
 % You are free to create new functions if you want.
+[height, width] = size(img_left);
+n_pixels = height*width;
 
+rng('default') % seeding for reproducibility
+labels = randi([0 15],1,n_pixels); % start with any random labeling
 
+K = 2;
+lambda = 20;
+d_max = 15;
+
+%% first 0-1 swap :
+indices = []; % remember indices of the considered pixels
+for i = 1:n_pixels
+    if (labels(i) == 0 || labels(i) == 1)
+       indices = [indices i];
+    end
+end
+
+% get the unary terms :
+n_swap_terms = size(indices,2);
+unary = zeros(2, n_swap_terms);
+for i = 1:n_swap_terms
+    % TODO check if axis in the "natural" sense !
+    % TODO consider a frame of length 15 where we cannot make the computation
+    col_idx = mod(indices(i), width);
+    row_idx = (indices(i)-col_idx)/width;
+    unary(i,0) = abs(img_left(row_idx, col_idx) - img_right(row_idx, col_idx));
+    unary(i,1) = abs(img_left(row_idx, col_idx) - img_right(row_idx, col_idx - 1));
+end
+
+    
+%%
 % TODO : plot the obtained disparity map
 % figure(3); clf(3);
 % imagesc(disparity_est); colormap(gray);
