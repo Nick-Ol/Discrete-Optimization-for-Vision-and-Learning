fc6a58de376040019345ab10aaee3dbe04e369f2
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-03-07 14:21:02 +0100

    Given a-b swap

---
+++ Assignment 1/swap.m
@@ -0,0 +1,35 @@
+function [new_labels,E_initial,E_optimal] = swap(img_left, img_right, old_labels, a, b, K, lambda)
+% effectuates the a-b swap
+
+% unary terms :
+[indices, unary] = computeUnary(img_left, img_right, old_labels, a, b, lambda, K);
+
+% pairwise terms :
+pairwise = zeros(2,2);
+pairwise(1,2) = computePairwise(a, b, K);
+pairwise(2,1) = pairwise(1,2);
+
+% edge_weights :
+n_ab_terms = size(indices,2); % nb of nodes in the graph
+edge_weights = zeros(n_ab_terms);
+for i = 1:n_ab_terms
+    for j = 1:n_ab_terms
+        y_p = indices(1,i);
+        x_p = indices(2,i);
+        y_q = indices(1,j);
+        x_q = indices(2,j);
+        if (isNeighbour(y_p,x_p,y_q,x_q))
+            edge_weights(i,j) = computeWeight(img_left,img_right,y_p,x_p,y_q,x_q,lambda);
+        end
+    end
+     fprintf('%i over %i\n', i, n_ab_terms);
+end
+
+% now optimize this binary MRF :
+[labels_ab,E_initial,E_optimal] = optimizeBinaryMRF(unary, edge_weights, pairwise);
+% replace the modified labels in old_labels :
+new_labels = old_labels;
+for i = 1:n_ab_terms
+    new_labels(indices(1,i),indices(2,i)) = labels_ab(i);
+end
+
