8209ade38e23002275cd97f25874c0e4511bda36
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-03-08 11:46:53 +0100

    abswap function
    Cannot test it

---
+++ Assignment 1/abswap.m
@@ -0,0 +1,14 @@
+function [new_labels] = abswap(img_left, img_right, old_labels, d_max, K, lambda, n_iter)
+% compute each ab swap, with a and b between 0 and d_max, n_iter times
+% TODO : add a stopping criterion
+
+comb = combnk(0:d_max,2)'; % all pairs of labels, 2*n_comb matrix
+n_comb = size(comb,2);
+
+for iter = 1:n_iter
+    for i = 1:n_comb
+        a = comb(1,i);
+        b = comb(2,i);
+        [new_labels,E_initial,E_optimal] = swap(img_left, img_right, old_labels, a, b, K, lambda);
+    end
+end    
